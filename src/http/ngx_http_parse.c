44c98d85cca6add793575d319124be091cb8ba56
Sergey Kandaurov <pluknet@nginx.com>
2026-05-08 19:38:43 +0400

    Reject HTTP CONNECT method with no port after colon (grafted from 2f17a17cc588c7fee47eaee68a0fb12dc49bb907)

--- src/http/ngx_http_parse.c
+++ src/http/ngx_http_parse.c
@@ -122,6 +122,7 @@ ngx_http_parse_request_line(ngx_http_request_t *r, ngx_buf_t *b)
         sw_host,
         sw_host_end,
         sw_host_ip_literal,
+        sw_port_start,
         sw_port,
         sw_after_slash_in_uri,
         sw_check_uri,
@@ -415,7 +416,7 @@ ngx_http_parse_request_line(ngx_http_request_t *r, ngx_buf_t *b)
         case sw_host_end:
 
             if (ch == ':') {
-                state = sw_port;
+                state = sw_port_start;
                 break;
             }
 
@@ -490,6 +491,19 @@ ngx_http_parse_request_line(ngx_http_request_t *r, ngx_buf_t *b)
                 return NGX_HTTP_PARSE_INVALID_REQUEST;
             }
             break;
+
+        case sw_port_start:
+            state = sw_port;
+
+            if (ch >= '0' && ch <= '9') {
+                break;
+            }
+
+            if (r->method == NGX_HTTP_CONNECT) {
+                return NGX_HTTP_PARSE_INVALID_REQUEST;
+            }
+
+            /* fall through */
 
         case sw_port:
             if (ch >= '0' && ch <= '9') {
