0df54e845ab011772d681462dfc27aa3fcea9294
hamjin <jinham@qq.com>
2026-05-15 00:48:12 +0800

    Core: Use OpenSSL's optimized MD5 and SHA1 implementations if available
    Signed-off-by: hamjin <jinham@qq.com>

--- src/core/ngx_md5.h
+++ src/core/ngx_md5.h
@@ -13,6 +13,20 @@
 #include <ngx_core.h>
 
 
+#if (NGX_OPENSSL)
+
+#include <openssl/evp.h>
+
+typedef struct {
+    EVP_MD_CTX  *ctx;
+} ngx_md5_t;
+
+void ngx_md5_init(ngx_md5_t *ctx);
+void ngx_md5_update(ngx_md5_t *ctx, const void *data, size_t size);
+void ngx_md5_final(u_char result[16], ngx_md5_t *ctx);
+
+#else
+
 typedef struct {
     uint64_t  bytes;
     uint32_t  a, b, c, d;
@@ -23,6 +37,8 @@ typedef struct {
 void ngx_md5_init(ngx_md5_t *ctx);
 void ngx_md5_update(ngx_md5_t *ctx, const void *data, size_t size);
 void ngx_md5_final(u_char result[16], ngx_md5_t *ctx);
+
+#endif
 
 
 #endif /* _NGX_MD5_H_INCLUDED_ */
